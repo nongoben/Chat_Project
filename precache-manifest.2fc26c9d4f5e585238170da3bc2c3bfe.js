d0eada39f73c1fe6e8f36f9e13f498c0655e1ced
nongoben <omyim_rabbit4@hotmail.com>
2020-08-31 10:27:47 +0700

    Updates

---
+++ precache-manifest.2fc26c9d4f5e585238170da3bc2c3bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc78e7f00e5ebdfe5a336e4abc9e04d2",
+    "url": "/Chat_Project/index.html"
+  },
+  {
+    "revision": "d3810778023e9a4777ce",
+    "url": "/Chat_Project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0b504a322afdd3926b98",
+    "url": "/Chat_Project/static/js/2.042f7073.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Chat_Project/static/js/2.042f7073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3810778023e9a4777ce",
+    "url": "/Chat_Project/static/js/main.d3d65bfb.chunk.js"
+  },
+  {
+    "revision": "bcc1fe7e7f45bd1496f0",
+    "url": "/Chat_Project/static/js/runtime-main.a2c20209.js"
+  }
+]);
